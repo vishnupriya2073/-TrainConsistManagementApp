52886ffbe3fac0250ad90e3e2ed718a9ab8753c0
vishnupriya2073 <vishnupriya.73sp@gmail.com>
2026-03-25 14:40:45 +0530

    feature/UC3

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,19 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+import java.util.*;
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
+public class Main {
+
+  public static void main(String[] args) {
+
+    System.out.println("=== Train Consist Management App ===");
+
+    Set<String> bogieIds = new HashSet<>();
+
+    bogieIds.add("BG101");
+    bogieIds.add("BG102");
+    bogieIds.add("BG103");
+    bogieIds.add("BG101");
+    bogieIds.add("BG102");
+
+    System.out.println("Unique Bogie IDs: " + bogieIds);
   }
 }
