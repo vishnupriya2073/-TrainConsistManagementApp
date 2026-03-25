2e589b5d5f33167f50431dc7c1723ab4b18c98e6
vishnupriya2073 <vishnupriya.73sp@gmail.com>
2026-03-25 14:44:17 +0530

    feature/UC4

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,24 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+import java.util.*;
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
+public class Main {
+
+  public static void main(String[] args) {
+
+    System.out.println("=== Train Consist Management App ===");
+
+    LinkedList<String> trainConsist = new LinkedList<>();
+
+    trainConsist.add("Engine");
+    trainConsist.add("Sleeper");
+    trainConsist.add("AC");
+    trainConsist.add("Cargo");
+    trainConsist.add("Guard");
+
+    trainConsist.add(2, "Pantry Car");
+
+    trainConsist.removeFirst();
+    trainConsist.removeLast();
+
+    System.out.println("Final Train Consist: " + trainConsist);
   }
 }
