d0ff4607f6dcd80a0660ed678087619b58dfa637
vishnupriya2073 <vishnupriya.73sp@gmail.com>
2026-03-25 15:50:29 +0530

    feature/UC7

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,31 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+import java.util.*;
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
+class Bogie {
+  String name;
+  int capacity;
+
+  Bogie(String name, int capacity) {
+    this.name = name;
+    this.capacity = capacity;
+  }
+}
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    System.out.println("=== Train Consist Management App ===");
+
+    List<Bogie> bogies = new ArrayList<>();
+
+    bogies.add(new Bogie("Sleeper", 72));
+    bogies.add(new Bogie("AC Chair", 56));
+    bogies.add(new Bogie("First Class", 24));
+
+    bogies.sort(Comparator.comparingInt(b -> b.capacity));
+
+    for (Bogie b : bogies) {
+      System.out.println(b.name + " Capacity: " + b.capacity);
+    }
   }
 }
