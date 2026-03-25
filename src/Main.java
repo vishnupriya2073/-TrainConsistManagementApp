75f767b725d252da503d53b9c7a70fcaa091dad8
vishnupriya2073 <vishnupriya.73sp@gmail.com>
2026-03-25 15:47:26 +0530

    feature/UC6

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,19 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+import java.util.*;
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
+public class Main {
+
+  public static void main(String[] args) {
+
+    System.out.println("=== Train Consist Management App ===");
+
+    HashMap<String, Integer> bogieCapacity = new HashMap<>();
+
+    bogieCapacity.put("Sleeper", 72);
+    bogieCapacity.put("AC Chair", 78);
+    bogieCapacity.put("First Class", 24);
+
+    for (Map.Entry<String, Integer> entry : bogieCapacity.entrySet()) {
+      System.out.println(entry.getKey() + " Capacity: " + entry.getValue());
+    }
   }
 }
