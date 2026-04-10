228499dd5858174d5e7b7381d1d6bbf8ca8c5241
vishnupriya2073 <vishnupriya.73sp@gmail.com>
2026-04-10 15:56:44 +0530

    UC18

--- src/UseCase17.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-public class UseCase17 {
-    public static void main(String[] args) {
-        String[] bogies = {"Sleeper","AC","General"};
-        Arrays.sort(bogies);
-        System.out.println(Arrays.toString(bogies));
-    }
-}
