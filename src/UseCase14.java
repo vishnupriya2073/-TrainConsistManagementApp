318a55ef6fed8b18b965801f1ac3335fdd4204da
vishnupriya2073 <vishnupriya.73sp@gmail.com>
2026-04-10 15:56:35 +0530

    UC14

---
+++ src/UseCase14.java
@@ -0,0 +1,10 @@
+public class UseCase14 {
+    public static void main(String[] args) {
+        int capacity = -10;
+        if (capacity < 0) {
+            System.out.println("Invalid Bogie Capacity");
+        } else {
+            System.out.println("Valid Capacity: " + capacity);
+        }
+    }
+}
